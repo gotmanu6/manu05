424b0b8b09081aad60326b60c315d8ac0ed51fb2
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-23 15:05:59 -0300

    Initial commit

---
+++ manu05/src/test/java/com/example/manu05/Manu05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manu05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Manu05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
