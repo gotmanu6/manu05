97d9d29a1c143594960d58b896857d6d6dc83ebc
Emanuelly <manu.presteslopes10@gmail.com>
2026-03-23 15:29:37 -0300

    repositories

--- manu05/src/main/java/repositories/CategoriaRepositorie.java
+++ manu05/src/main/java/repositories/CategoriaRepositorie.java
@@ -2,6 +2,5 @@ package repositories;
 
 import models.CategoriaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface CategoriaRepositorie extends JpaRepository<CategoriaModel, Long> {
 }
